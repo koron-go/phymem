ae7c63089b437dc52710a1089de9fcd1316be8e5
MURAOKA Taro <koron.kaoriya@gmail.com>
2020-03-15 13:07:05 +0900

    misc fix

--- phymem.go
+++ phymem.go
@@ -8,9 +8,9 @@ import "errors"
 // for test.
 const providedCurrent = false
 
-var notImpl = errors.New("Current() is not implemented for this platform")
+var errNotImpl = errors.New("Current() is not implemented for this platform")
 
 // Current get physical memory which used by current process.
 func Current() (uint, error) {
-	return 0, notImpl
+	return 0, errNotImpl
 }
