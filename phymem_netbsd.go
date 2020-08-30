b68193327234e6c145cc4dbae78f3b54ae2dda3a
Ryo ONODERA <ryo@tetera.org>
2020-08-30 21:17:52 +0900

    Add NetBSD support
    * Return value of getRSS() is too short for 64-bit environment.
      This will fixed in my next commit.

---
+++ phymem_netbsd.go
@@ -0,0 +1,41 @@
+package phymem
+
+/*
+#include <stdio.h>
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <unistd.h>
+
+static int
+getRSS(void)
+{
+    int ret;
+    size_t len;
+
+    struct kinfo_proc2 kp;
+    int mib[] = {CTL_KERN, KERN_PROC2, KERN_PROC_PID, getpid(), sizeof(kp), 1};
+    size_t miblen = __arraycount(mib);
+    len = sizeof(kp);
+    ret = sysctl(mib, miblen, &kp, &len, NULL, 0);
+    if (ret < 0) {
+        return -1;
+    }
+
+    long pagesize = sysconf(_SC_PAGESIZE);
+
+    return kp.p_vm_rssize * pagesize;
+}
+*/
+import "C"
+import "fmt"
+
+// for test.
+const providedCurrent = true
+
+func Current() (uint, error){
+    rss := int(C.getRSS())
+    if rss < 0 {
+        return 0, fmt.Errorf("failed to get RSS via sysctl: %d", rss)
+    }
+    return uint(rss), nil
+}
